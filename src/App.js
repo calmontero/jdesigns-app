afe1986298c78b3c4472f4c6ae7dfa1d7cadcbd7
calmontero <carlos.alfonso.montero@gmail.com>
2022-07-05 18:41:38 -0400

    Fixing deploy in Netlify test02

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Navigation from './components/Navigation/Navigation';
-import Services from './components/pages/Services';
+//import Services from './components/pages/Services';
 import CraftSwiper from './components/CraftSwiper/CraftSwiper';
-//import Downloads from './components/pages/Downloads';
-//import Testimonial from './components/Testimonial/Testimonial';
+import Downloads from './components/pages/Downloads';
+import Testimonial from './components/Testimonial/Testimonial';
 
 function App() {
   return (
@@ -15,10 +15,9 @@ function App() {
         <Navigation />
         <Switch>
           <Route path='/' exact component={Home} />
-          <Route path='/testimonial' exact component={Services} />
-          <Route path='/downloads' exact component={Services} />
-          {/*<Route path='/testimonial' exact component={Testimonial} />*/}
-          {/*<Route path='/downloads' exact component={Downloads} />*/}
+          {/*<Route path='/' exact component={Services} />*/}
+          <Route path='/testimonial' exact component={Testimonial} />
+          <Route path='/downloads' exact component={Downloads} />
           <Route path='/services/:id' exact component={CraftSwiper} />
         </Switch>
       </Router>
